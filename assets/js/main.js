90e36c222509e0e773cef0bb762ab41264aa58c3
Isabel Pereira <isabelalvaro116@gmail.com>
2023-01-19 22:34:34 +0000

    added object into nlwSetup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-document.querySelector('input').click()
+const form=document.querySelector('form')
+const nlwSetup= new NLWSetup(form)
+
+const data = {
+  run: ["01-01", "01-02", "01-06"],
+  water: ["01-04", "01-05"],
+  food: ["01-01", "01-03"],
+}
+nlwSetup.setData(data)
