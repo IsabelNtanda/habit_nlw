0947ea87a621b2eccdb5659f90e11e956af1e556
Isabel Pereira <isabelalvaro116@gmail.com>
2023-01-20 20:30:46 +0000

    card reduced

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,6 @@ const nlwSetup= new NLWSetup(form)
 
 const data = {
   run: ["01-01", "01-02", "01-06"],
-  water: ["01-04", "01-05"],
-  food: ["01-01", "01-03"],
 }
 nlwSetup.setData(data)
 nlwSetup.load()
