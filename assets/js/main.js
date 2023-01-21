90238d2de314cc6188c4473b89de6d72ba65a618
Isabel Pereira <isabelalvaro116@gmail.com>
2023-01-21 19:34:06 +0000

    added new date function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,27 @@
-const form=document.querySelector('form')
-const nlwSetup= new NLWSetup(form)
+const form = document.querySelector('form')
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add(){
+const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+const dayExists = nlwSetup.dayExists(today)
+
+if (dayExists) {
+  alert("Dia já incluso ❌" )
+  return 
+}
+alert('Dia adicionado com sucesso ✅ ')
+nlwSetup.addDay(today)
+}
+function save() {
+  
+}
 
 const data = {
-  run: ["01-01", "01-02", "01-06"],
+  run: ["01-01"],
 }
 nlwSetup.setData(data)
 nlwSetup.load()
